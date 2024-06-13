b7cae8c5ba959b9e316ba757735c5539a9aa2754
Kary6710 <lopezk6710@gmail.com>
2024-06-13 11:30:27 -0500

    Update wage calculator.py

--- wage calculator.py
+++ wage calculator.py
@@ -1,6 +1,8 @@
 #calculate the yearly wage given the two inputs, hours worked daily and hourly wage. multiplying hours worked daily and hourly wage gives you the ages earned in a day
-#function header
-def get_poitive_hourly_wage_input():
+#function header 
+
+
+def get_positive_hourly_wage_input():
 
     #Note that the workers hours is daily. 12%
     user_hourly_wage =0
@@ -39,21 +41,44 @@ def get_positives_Hours_worked_daily_input():
 
     return user_hours_worked_daily
 
+#make a loop to ask the user if they would like to run the program again.
 
-#INPUT
-user_hourly_wage = get_poitive_hourly_wage_input()
-user_hours_worked_daily = get_positives_Hours_worked_daily_input()
 
-#Processing
-wages_before_tax = user_hourly_wage * user_hours_worked_daily*365
-tax = .12 * wages_before_tax
-wages_after_tax =  wages_before_tax - tax 
-#OUTPUT
-print(f"             ")
-print(f"  PAY ADVICE")
-print(f"----------------")
-print(f"Hours worked: ${user_hours_worked_daily}")
-print(f"Hourly Wage: ${user_hourly_wage}")
-print(f"Wages Before Taxes: ${wages_before_tax:.2f}")
-print(f"Tax: {tax:.2f}")
-print(f"Wages After Tax: ${wages_after_tax:.2f}")
+#create a main function to put the rest of the code.
+def main():
+
+    while(True):
+
+        #INPUT
+        user_hourly_wage = get_positive_hourly_wage_input()
+        user_hours_worked_daily = get_positives_Hours_worked_daily_input()
+
+        #Processing
+        wages_before_tax = user_hourly_wage * user_hours_worked_daily*365
+        tax = .12 * wages_before_tax
+        wages_after_tax =  wages_before_tax - tax 
+
+        #OUTPUT
+        print(f"             ")
+        print(f"  PAY ADVICE")
+        print(f"----------------")
+        print(f"Hours worked: ${user_hours_worked_daily}")
+        print(f"Hourly Wage: ${user_hourly_wage}")
+        print(f"Wages Before Taxes: ${wages_before_tax:.2f}")
+        print(f"Tax: {tax:.2f}")
+        print(f"Wages After Tax: ${wages_after_tax:.2f}")
+
+        user_answer = str(input("Would you like to run the calculator again? Enter 'y' to continue."))
+        #create code to make Yes or No break or continue 
+        if user_answer == "y":
+            continue
+        else:
+            print("Goodbye")
+            break
+
+
+        #call main function to run the program and/or write perameaters
+
+main()
+
+
