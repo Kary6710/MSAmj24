c973d56ee6f77467eb85011e836f12aac0bc1d00
Kary6710 <lopezk6710@gmail.com>
2024-06-17 09:08:31 -0500

    Update vending_ machine.py

--- vending_ machine.py
+++ vending_ machine.py
@@ -4,19 +4,37 @@ def main():
     coin = 0
     start_value = 50
     amount_due = start_value - coin
-#prompt the user to enter a coin 1,5,10, 25 then make sure it only excepts thos numbers
+
+#prompt the user to enter a coin 1,5,10, 25 then make sure it only excepts those numbers
+    print("Vending Machine")
+    print("---------------------- \n")
     while(True):
-        try:
-            print(f"Amount due is: {amount_due} ")
-            coin = int(input("Enter a coin (1, 5, 10, and 25 only): "))
-            if coin == 1 or coin == 5 or coin == 10 or coin == 25:
-              break
-            else:
+        while(True):
+            try:
+                print(f"Amount due is: {amount_due} ")
+                coin = int(input("Enter a coin (1, 5, 10, and 25 only): "))
+                if coin == 1 or coin == 5 or coin == 10 or coin == 25:
+                    
+                #process the input and display the updated amount.
+                #coin - start value = amount due
+
+                    amount_due = start_value - coin
+                    start_value = amount_due
+                    print("\n")
+                    break
+                else:
+                    continue
+            except:
                 continue
-        except:
-            continue
+                # stop program at 0 
+                # If amount is (-) something print returning... amount_ due and break
 
-#process the input and display the updated amount.
-#coin - start value = amount due
+        if amount_due == 0:
+            print("Amount due is: 0")
+            break
+        elif amount_due < 0:
+            print(f"Returning... {abs(amount_due)}")
+            break
+        else: continue
 
-main()
+main() 
