5e4d720d0955272284256d02d40d81a0e08eb77e
Kary6710 <lopezk6710@gmail.com>
2024-06-20 11:03:20 -0500

    Update adding_game.py

--- adding_game.py
+++ adding_game.py
@@ -1,25 +1,39 @@
-# 1)prompt user for difficulty level 
+# 1prompt user for difficulty level 
 #create a function for game level
 #prompt the user again if the do not pick a difficulty level 1-3
 #set a range for diff_level 1, 2, and 3
 
-def level_input():
+def get_level():
     while(True):
-            diff_level = int(input("Select difficulty level 1, 2, or 3:"))
-            if diff_level == 1 or diff_level ==2 or diff_level == 3:
-                 return diff_level
-                 break
-            else: 
-                print("Invalid input!")
-                continue  
-level_input()
+            try:
+                diff_level = int(input("Select difficulty level 1, 2, or 3:"))
+                if diff_level == 1 or diff_level ==2 or diff_level == 3:
+                    return diff_level
+                    break
+                else: 
+                    print("Invalid input!")
+                    continue  
+            except:
+                continue
 
 
-#2.)create a function for number of questions
+#2.create a function for number of questions
 #prompt the user for the number of questions to ask
 #prompt the user again if they do not input a number 3-10
+def get_number_of_questions():
+    while(True):
+        try:
+            attempts = int(input("How many questions would you like from 3-10:"))
+            if 10 >= attempts >= 3:
+                return attempts
+            else:
+                continue
+        except: 
+            continue
 
-#3)create main function
+
+
+#3create main function
 #create a for loop to genrate the code number_of_times the user inputed
 # The format for the problems are X + Y = 
 # X and Y cn not be negative and must be random 
@@ -32,3 +46,54 @@ level_input()
 # output the precentage by dividing accuracy by number_of _times then multiply by 100 and formatted to 2 decimal places
 #end program
 
+
+import random
+def main():
+    diff = get_level()
+    questions = get_number_of_questions()
+    acurracy = 0
+    attempts = 0
+    for number in range (questions):
+      
+        if diff == 1:
+            x = random.randrange(0,10) 
+            y = random.randrange(0,10)
+        elif diff == 2:
+            x = random.randrange(10,99) 
+            y = random.randrange(10,99)
+        elif diff == 3:
+            x = random.randrange(100,999) 
+            y = random.randrange(100,999)
+
+        answer = x + y
+        incorrect = 1 
+
+        while (True):
+            try:
+                user_input = int(input(f"{x} + {y} = "))
+                if incorrect == 3:
+                    print(f"{x} + {y} = {answer} ")
+                    attempts += 1
+                    break
+                if user_input != answer:
+                    incorrect += 1
+                    print("incorrect")
+                    continue
+                else: 
+                    acurracy += 1
+                    attempts += 1
+                    print("Correct!!")
+                    break
+            except:
+                incorrect += 1
+                print("incorrect")
+                continue
+        if attempts == questions:
+            decimal_perecnt = acurracy / questions
+            percent = decimal_perecnt * 100
+            print(f" Your accuracy is {percent:.2f}%")
+            break
+            
+            
+          
+main()
